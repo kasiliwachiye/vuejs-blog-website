8a2ec97b99c5a710c229ea9b4f2d74c1aa1916f2
Hari <manjaka.rajaonson@gmail.com>
2022-06-22 20:39:15 +0300

    add social footer

---
+++ src/components/SocialArea.vue
@@ -0,0 +1,47 @@
+<template>
+	<footer class="social-area">
+		<ul class="social-area__list container">
+			<li v-for="(item, key) in list" :key="key">
+				<a class="link" :href="item.link" :title="item.title">
+					<Icon :icon="item.icon" />
+				</a>
+			</li>
+		</ul>
+	</footer>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/*
+    Navigation and filter
+*/
+
+export default {
+	name: "SocialArea",
+	components: {
+		Icon: FontAwesomeIcon,
+	},
+	data() {
+		return {
+			list: [
+				{
+					title: "View my Facebook profile",
+					link: "https://www.facebook.com/Manjaka13",
+					icon: ["fab", "facebook"],
+				},
+				{
+					title: "Check my repos",
+					link: "https://github.com/Manjaka13",
+					icon: ["fab", "github"],
+				},
+				{
+					title: "Connect with LinkedIn",
+					link: "https://www.linked.com/in/harijaona-rajaonson",
+					icon: ["fab", "linkedin"],
+				},
+			],
+		};
+	},
+};
+</script>
