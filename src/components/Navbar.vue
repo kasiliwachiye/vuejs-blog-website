7bb5a0ad8631b5b8e0f746efd72db03ba5295351
Hari <manjaka.rajaonson@gmail.com>
2022-06-22 17:38:45 +0300

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+	<nav class="navbar">
+		<div class="navbar__container container">
+			<div class="navbar__icon-box">
+				<Icon class="icon" :icon="['fas', 'newspaper']" />
+			</div>
+			<h1 class="navbar__title">Expat News</h1>
+		</div>
+	</nav>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/*
+    Navigation and filter
+*/
+
+export default {
+	name: "NavBar",
+	components: {
+		Icon: FontAwesomeIcon,
+	},
+};
+</script>
