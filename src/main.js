45581ea1e0511a9c45dc7773fca31a94518a6e34
Hari <manjaka.rajaonson@gmail.com>
2022-06-23 15:07:29 +0300

    add lazy loading support

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import Home from "@/components/Home.vue";
 import "@/helpers/icons.js";
 import "@/styles/index.scss";
 import store from "@/store";
+import VueLazyLoad from "vue-lazyload";
 
 /*
 	Entry point
 */
 
 Vue.config.productionTip = false;
+Vue.use(VueLazyLoad, {
+	preLoad: 1.3,
+	loading: "/assets/loading.gif",
+	error: "/assets/error.jpg"
+});
 
 new Vue({
 	render: (h) => h(Home),
