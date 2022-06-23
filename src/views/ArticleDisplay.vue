f6d98532013aba798af06c6e8ec9ab336d079a38
Hari <manjaka.rajaonson@gmail.com>
2022-06-23 20:58:36 +0300

    update article page display

--- src/views/ArticleDisplay.vue
+++ src/views/ArticleDisplay.vue
@@ -2,9 +2,9 @@
 	<div class="article-display">
 		<div v-if="article">
 			<div class="article-display__head">
-				<h1 class="article-display__title">{{ processTitle() }}</h1>
+				<h1 class="article-display__title">{{ article.title }}</h1>
 				<figure class="article-display__image">
-					<img class="image" v-lazy="`/assets/test.webp`" :alt="processTitle()" />
+					<img class="image" v-lazy="article.image" :alt="article.title" />
 				</figure>
 			</div>
 			<p class="article-display__intro" v-html="processDescription()"></p>
@@ -17,6 +17,11 @@
 <script>
 import { mapGetters } from "vuex";
 import LoadingNews from "@/components/LoadingNews.vue";
+import {
+	extractDescription,
+	extractDate,
+	extractBody,
+} from "@/helpers/utils.js";
 
 /*
 	Article page
@@ -35,41 +40,22 @@ export default {
 	},
 	// Get store variables we use here
 	computed: mapGetters(["news"]),
-	watch: {
-		// news() {
-		// 	if (this.news.length > 0) {
-		// 		const id = this.$route.params.id;
-		// 		for (let i = 0; i < this.news.length; i++)
-		// 			if (this.news[i].id === parseInt(id)) {
-		// 				this.article = { ...this.news[i] };
-		// 				break;
-		// 			}
-		// 	}
-		// },
-	},
 	methods: {
+		// Callback when loading finished
 		finishedLoading(article) {
-			console.log(article);
+			this.article = article;
 		},
-		// Remove p tag because parent is already a p tag
+		// Extract description content
 		processDescription() {
-			return this.article.introduction.replace("<p>", "").replace("</p>", "");
-		},
-		// Adjust spacing
-		processTitle() {
-			return this.article.title.replace("?", " ?").replace("!", " !");
+			return extractDescription(this.article.introduction);
 		},
 		// Only display date, not time
 		processDate() {
-			return this.date.split(" ")[0].split("-").reverse().join("-");
+			return extractDate(this.article.date);
 		},
+		// Extracts body content
 		processBody() {
-			return this.article.body
-				.replace(
-					'<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">\n<html><body><p>',
-					""
-				)
-				.replace("</p>\r\n</body></html>\n", "");
+			return extractBody(this.article.body);
 		},
 	},
 };
