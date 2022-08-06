cd61aa313a0da607d12df58d94ca58e04cceef89
Dimby Rasolonirina <dimbyrasolonirina@gmail.com>
2022-08-06 22:29:47 +0300

    change :style to :class

--- src/components/blocArticle.vue
+++ src/components/blocArticle.vue
@@ -1,24 +1,16 @@
 <template>
     <div>
-        <div class="articleItem">
+        <div class="article-item">
             <img :src="item.images" v-html="item.images"/>
-            <div class="content">
+            <div class="content" :class="[item.type]">
                 <div v-html="item.type"></div>
                 <div 
                     v-html="item.title"
-                    :style="{
-                        'display': '-webkit-box',
-                        '-webkit-line-clamp': 3,
-                        '-webkit-box-orient': 'vertical',
-                        'overflow': 'hidden'}"
+                    :class="['article-title']"
                 ></div>
                 <div 
                     v-html="item.introduction" 
-                    :style="{
-                        'display': '-webkit-box',
-                        '-webkit-line-clamp': 2,
-                        '-webkit-box-orient': 'vertical',
-                        'overflow': 'hidden'}"
+                    :class="['article-introduction']"
                 ></div>
                 <div v-html="item.date"></div>
             </div>
@@ -33,15 +25,27 @@ export default {
 }
 </script>
 <style scoped>
-    .articleItem {
+    .article-item {
         border: 1px solid #ededed;
         width: 250px;
         margin: 0 10px 20px 10px;
         height: 300px;
         border-radius: 8px;
     }
-    .articleItem:hover {
+    .article-item:hover {
         border: 1px solid rgb(145, 141, 141);
+    }
+    .article-title {
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
+    .article-introduction {
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
     }
     img {
         width: 250px;
