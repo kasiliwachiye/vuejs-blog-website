992bb3077c9c3e71fd2b6ca2b31335ed32bfd6e6
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-08-18 16:06:25 +0300

    feat: add background svg

--- src/components/ArticleDetails.vue
+++ src/components/ArticleDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="article-details-container">
     <NavbarVue />
     <div
       class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
@@ -14,14 +14,14 @@
           ></p>
         </div>
         <h2
-          class="max-w-lg mb-6 text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto uppercase"
+          class="max-w-lg mb-6 text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto uppercase title-text"
           v-html="article.title"
         ></h2>
-        <p class="text-gray-600" v-html="article.date"></p>
         <p
-          class="text-base text-gray md:text-lg"
+          class="text-base text-gray md:text-lg intro-text"
           v-html="article.introduction"
         ></p>
+        <p class="text-gray-600 date-text" v-html="article.date"></p>
       </div>
       <div class="mx-auto lg:max-w-2xl">
         <div
@@ -34,7 +34,7 @@
           />
         </div>
         <p
-          class="text-base mt-5 text-gray md:text-lg"
+          class="text-base mt-5 text-gray md:text-lg body-text"
           v-html="article.body"
         ></p>
       </div>
@@ -66,4 +66,26 @@ export default {
   },
 };
 </script>
-<style></style>
+<style scoped>
+@font-face {
+  font-family: "Neue Haas Display Light";
+  src: url(../fonts/NeueHaasDisplayLight.ttf);
+}
+
+.article-details-container {
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.com/svgjs' width='1366' height='768' preserveAspectRatio='none' viewBox='0 0 1366 768'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1104%26quot%3b)' fill='none'%3e%3cpath d='M630.5725093590693 344.81128618442114L623.9662162495124 155.63180343796972 434.786733503061 162.23809654752657 441.39302661261786 351.41757929397795z' fill='rgba(194%2c 223%2c 255%2c 0.4)' class='triangle-float1'%3e%3c/path%3e%3cpath d='M143.93222376566118 264.98568887343595L197.66728004861616 380.2208889462034 312.90248012138363 326.4858326632484 259.1674238384286 211.25063259048096z' fill='rgba(194%2c 223%2c 255%2c 0.4)' class='triangle-float2'%3e%3c/path%3e%3cpath d='M1087.3642064558533 507.21853100016733L1120.0230902541566 354.28737186789795 989.2959802481428 394.25466029515786z' fill='rgba(194%2c 223%2c 255%2c 0.4)' class='triangle-float1'%3e%3c/path%3e%3cpath d='M365.8153734061058 413.14311430780197L385.8078820601514 526.5262651660403 545.8863540204861 439.8459267558526z' fill='rgba(194%2c 223%2c 255%2c 0.4)' class='triangle-float2'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1104'%3e%3crect width='1366' height='768' fill='white'%3e%3c/rect%3e%3c/mask%3e%3cstyle%3e %40keyframes float1 %7b 0%25%7btransform: translate(0%2c 0)%7d 50%25%7btransform: translate(-10px%2c 0)%7d 100%25%7btransform: translate(0%2c 0)%7d %7d .triangle-float1 %7b animation: float1 5s infinite%3b %7d %40keyframes float2 %7b 0%25%7btransform: translate(0%2c 0)%7d 50%25%7btransform: translate(-5px%2c -5px)%7d 100%25%7btransform: translate(0%2c 0)%7d %7d .triangle-float2 %7b animation: float2 4s infinite%3b %7d %40keyframes float3 %7b 0%25%7btransform: translate(0%2c 0)%7d 50%25%7btransform: translate(0%2c -10px)%7d 100%25%7btransform: translate(0%2c 0)%7d %7d .triangle-float3 %7b animation: float3 6s infinite%3b %7d %3c/style%3e%3c/defs%3e%3c/svg%3e");
+}
+
+.date-text {
+  text-align: right;
+}
+
+.intro-text {
+  text-align: left;
+}
+
+.body-text {
+  font-family: "Neue Haas Display Light";
+  text-align: left;
+}
+</style>
--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="articles-container">
     <NavbarVue />
     <!-- inputs -->
     <div class="m-2 flex justify-center">
@@ -48,18 +48,8 @@
       </div>
     </div>
     Sort
-    <button
-      class="btn btn-outline m-2"
-      @click="sortDesc"
-    >
-      Recent
-    </button>
-    <button
-      class="btn btn-outline m-2"
-      @click="sortAsc"
-    >
-      Older
-    </button>
+    <button class="btn btn-outline m-2" @click="sortDesc">Recent</button>
+    <button class="btn btn-outline m-2" @click="sortAsc">Older</button>
     <!-- articles -->
     <div
       class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
@@ -67,6 +57,7 @@
       <div
         class="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full"
       >
+        <p v-if="filteredData.length < 1">No articles found 💩</p>
         <ArticleCard
           :article="item"
           v-for="(item, key) in filteredData"
@@ -133,4 +124,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.articles-container {
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.com/svgjs' width='1366' height='768' preserveAspectRatio='none' viewBox='0 0 1366 768'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1116%26quot%3b)' fill='none'%3e%3cpath d='M1247.768%2c354.177C1269.411%2c354.07%2c1289.839%2c342.678%2c1300.068%2c323.605C1309.807%2c305.446%2c1305.34%2c283.964%2c1294.976%2c266.154C1284.685%2c248.468%2c1268.206%2c234.722%2c1247.768%2c233.725C1225.546%2c232.641%2c1203.13%2c241.949%2c1191.846%2c261.123C1180.42%2c280.539%2c1182.963%2c304.815%2c1194.568%2c324.125C1205.813%2c342.836%2c1225.938%2c354.285%2c1247.768%2c354.177' fill='rgba(194%2c 223%2c 255%2c 0.4)' class='triangle-float3'%3e%3c/path%3e%3cpath d='M1172.559707651228 257.71003882755883L1119.583504248666 103.37832962642383 1024.1406807375656 209.378323868486z' fill='rgba(194%2c 223%2c 255%2c 0.4)' class='triangle-float1'%3e%3c/path%3e%3cpath d='M640.9790276039561 458.345020486133L738.68206997263 621.3017671404481 821.7279149953582 477.4621442035918z' fill='rgba(194%2c 223%2c 255%2c 0.4)' class='triangle-float1'%3e%3c/path%3e%3cpath d='M210.64 430.75 a177.95 177.95 0 1 0 355.9 0 a177.95 177.95 0 1 0 -355.9 0z' fill='rgba(194%2c 223%2c 255%2c 0.4)' class='triangle-float3'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1116'%3e%3crect width='1366' height='768' fill='white'%3e%3c/rect%3e%3c/mask%3e%3cstyle%3e %40keyframes float1 %7b 0%25%7btransform: translate(0%2c 0)%7d 50%25%7btransform: translate(-10px%2c 0)%7d 100%25%7btransform: translate(0%2c 0)%7d %7d .triangle-float1 %7b animation: float1 5s infinite%3b %7d %40keyframes float2 %7b 0%25%7btransform: translate(0%2c 0)%7d 50%25%7btransform: translate(-5px%2c -5px)%7d 100%25%7btransform: translate(0%2c 0)%7d %7d .triangle-float2 %7b animation: float2 4s infinite%3b %7d %40keyframes float3 %7b 0%25%7btransform: translate(0%2c 0)%7d 50%25%7btransform: translate(0%2c -10px)%7d 100%25%7btransform: translate(0%2c 0)%7d %7d .triangle-float3 %7b animation: float3 6s infinite%3b %7d %3c/style%3e%3c/defs%3e%3c/svg%3e");
+}
+</style>
